09917744b7c525428d1879e9a2eebf2a882585a8
anurak <anurak@everapp.io>
2022-06-05 20:14:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test Deploy Netlify React Repo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <>Milo Test Deploy</>
       </header>
     </div>
   );
