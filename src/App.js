a5fa329cab6d132b87afe0a6e9596d294adcb388
anurak <anurak@everapp.io>
2022-06-05 20:22:03 +0700

    update typing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> LOVE P'GIFT AND TUKTIK FOREVER</h1>
         <p>
           Test Deploy Netlify React Repo
         </p>
